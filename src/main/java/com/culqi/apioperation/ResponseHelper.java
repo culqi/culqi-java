ac1e46a9bba0eeef8e5167df7d13431787900fe1
Tekton <lorenaortizdezevallos@MacBook-Pro-de-Lorena.local>
2024-01-03 09:31:55 -0500

    fix: java headers

--- src/main/java/com/culqi/apioperation/ResponseHelper.java
+++ src/main/java/com/culqi/apioperation/ResponseHelper.java
@@ -67,11 +67,19 @@ public class ResponseHelper {
         String result = "";
         try {
             String api_key = url.contains("tokens") ||  url.contains("confirm") ? Culqi.public_key : Culqi.secret_key;
+            String env = Config.X_CULQI_ENV_TEST;
+            if(api_key.contains("live")) {
+                env = Config.X_CULQI_ENV_LIVE;
+            }
             String base_url = url.contains("tokens") ? config.API_SECURE : config.API_BASE;
             RequestBody body = RequestBody.create(JSON, jsonData);
             Request request = new Request.Builder()
                     .url(base_url+url)
                     .header("Authorization","Bearer " + api_key)
+                    .header("x-culqi-env", env)
+                    .header("x-culqi-client", Config.X_CULQI_CLIENT)
+                    .header("x-culqi-client-version", Config.X_CULQI_CLIENT_VERSION)
+                    .header("x-api-version", Config.X_API_VERSION)
                     .post(body)
                     .build();
             Response response = client.newCall(request).execute();
@@ -86,12 +94,20 @@ public class ResponseHelper {
         String result = "";
         try {
             String api_key = url.contains("tokens") ||  url.contains("confirm") ? Culqi.public_key : Culqi.secret_key;
+            String env = Config.X_CULQI_ENV_TEST;
+            if(api_key.contains("live")) {
+                env = Config.X_CULQI_ENV_LIVE;
+            }
             String base_url = url.contains("tokens") ? config.API_SECURE : config.API_BASE;
             RequestBody body = RequestBody.create(JSON, jsonData);
             Request request = new Request.Builder()
                     .url(base_url+url)
                     .header("Authorization","Bearer " + api_key)
                     .header("x-culqi-rsa-id", rsaId)
+                    .header("x-culqi-env", env)
+                    .header("x-culqi-client", Config.X_CULQI_CLIENT)
+                    .header("x-culqi-client-version", Config.X_CULQI_CLIENT_VERSION)
+                    .header("x-api-version", Config.X_API_VERSION)
                     .post(body)
                     .build();
             Response response = client.newCall(request).execute();
@@ -104,11 +120,20 @@ public class ResponseHelper {
 
     public ResponseCulqi update(String url, String jsonData, String id) {
         String result = "";
-        try {System.out.println(config.API_BASE+url+id);
+        try {
+            String env = Config.X_CULQI_ENV_TEST;
+            if(Culqi.secret_key.contains("live")) {
+                env = Config.X_CULQI_ENV_LIVE;
+            }
+            System.out.println(config.API_BASE+url+id);
             RequestBody body = RequestBody.create(JSON, jsonData);
             Request request = new Request.Builder()
                     .url(config.API_BASE+url+id)
                     .header("Authorization","Bearer " + Culqi.secret_key)
+                    .header("x-culqi-env", env)
+                    .header("x-culqi-client", Config.X_CULQI_CLIENT)
+                    .header("x-culqi-client-version", Config.X_CULQI_CLIENT_VERSION)
+                    .header("x-api-version", Config.X_API_VERSION)
                     .patch(body)
                     .build();
             Response response = client.newCall(request).execute();
@@ -122,11 +147,19 @@ public class ResponseHelper {
     public ResponseCulqi update(String url, String jsonData, String id,  String rsaId) {
         String result = "";
         try {
+            String env = Config.X_CULQI_ENV_TEST;
+            if(Culqi.secret_key.contains("live")) {
+                env = Config.X_CULQI_ENV_LIVE;
+            }
             RequestBody body = RequestBody.create(JSON, jsonData);
             Request request = new Request.Builder()
                     .url(config.API_BASE+url+id)
                     .header("Authorization","Bearer " + Culqi.secret_key)
                     .header("x-culqi-rsa-id", rsaId)
+                    .header("x-culqi-env", env)
+                    .header("x-culqi-client", Config.X_CULQI_CLIENT)
+                    .header("x-culqi-client-version", Config.X_CULQI_CLIENT_VERSION)
+                    .header("x-api-version", Config.X_API_VERSION)
                     .patch(body)
                     .build();
             Response response = client.newCall(request).execute();
@@ -140,10 +173,18 @@ public class ResponseHelper {
     public ResponseCulqi get_or_delete(String url, String id, boolean delete) {
         String result = "";
         try {
+            String env = Config.X_CULQI_ENV_TEST;
+            if(Culqi.secret_key.contains("live")) {
+                env = Config.X_CULQI_ENV_LIVE;
+            }
             Request.Builder builder = new Request.Builder();
             builder.url(config.API_BASE + url + id);
             System.out.println(config.API_BASE + url + id);
-            builder.header("Authorization","Bearer " + Culqi.secret_key);
+            builder.header("Authorization","Bearer " + Culqi.secret_key)
+                .header("x-culqi-env", env)
+                .header("x-culqi-client", Config.X_CULQI_CLIENT)
+                .header("x-culqi-client-version", Config.X_CULQI_CLIENT_VERSION)
+                .header("x-api-version", Config.X_API_VERSION);
             if (delete) {
                 builder.delete();
             }
@@ -159,10 +200,18 @@ public class ResponseHelper {
     public ResponseCulqi capture(String url, String id) throws Exception {
         String result = "";
         try {
+            String env = Config.X_CULQI_ENV_TEST;
+            if(Culqi.secret_key.contains("live")) {
+                env = Config.X_CULQI_ENV_LIVE;
+            }
             RequestBody body = RequestBody.create(JSON, "");
             Request.Builder builder = new Request.Builder();
             builder.url(config.API_BASE + url + id + "/capture/");
-            builder.header("Authorization", "Bearer " + Culqi.secret_key);
+            builder.header("Authorization", "Bearer " + Culqi.secret_key)
+                    .header("x-culqi-env", env)
+                    .header("x-culqi-client", Config.X_CULQI_CLIENT)
+                    .header("x-culqi-client-version", Config.X_CULQI_CLIENT_VERSION)
+                    .header("x-api-version", Config.X_API_VERSION);
             builder.post(body);
             Request request = builder.build();
             Response response = client.newCall(request).execute();
@@ -176,10 +225,18 @@ public class ResponseHelper {
     public ResponseCulqi confirm(String url, String id) throws Exception {
         String result = "";
         try {
+            String env = Config.X_CULQI_ENV_TEST;
+            if(Culqi.public_key.contains("live")) {
+                env = Config.X_CULQI_ENV_LIVE;
+            }
             RequestBody body = RequestBody.create(JSON, "");
             Request.Builder builder = new Request.Builder();
             builder.url(config.API_BASE+url+id+"/confirm/");
-            builder.header("Authorization","Bearer " + Culqi.public_key);
+            builder.header("Authorization","Bearer " + Culqi.public_key)
+            .header("x-culqi-env", env)
+            .header("x-culqi-client", Config.X_CULQI_CLIENT)
+            .header("x-culqi-client-version", Config.X_CULQI_CLIENT_VERSION)
+            .header("x-api-version", Config.X_API_VERSION);
             builder.post(body);
             Request request = builder.build();
             Response response = client.newCall(request).execute();
